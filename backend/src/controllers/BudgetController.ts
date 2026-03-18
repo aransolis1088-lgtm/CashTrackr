16c29d15c94d3a6bf33ae578e66a218eedcb73e9
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-03-17 22:14:42 -0600

    Implementación REST APIS -  Creación router de Budget

---
+++ backend/src/controllers/BudgetController.ts
@@ -0,0 +1,42 @@
+import type { Request, Response } from 'express'
+import Budget from '../models/Budget'
+
+export class BudgetController {
+    static getAll = async (req: Request, res: Response) => {
+        try {
+            const budgets = await Budget.findAll({
+                order: [
+                    ['createdAt', 'DESC']
+                ],
+                //TODO: Filtrar por el usuario autenticado
+            })
+            res.json(budgets)
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error' })
+
+        }
+    }
+
+    static create = async (req: Request, res: Response) => {
+        try {
+            const budget = new Budget(req.body)
+            await budget.save()
+            res.status(201).json('Presupuesto Creado Correctamente')
+        } catch (error) {
+            // console.log(error)
+            res.status(500).json({ error: 'Hubo un error' })
+        }
+    }
+
+    static getById = async (req: Request, res: Response) => {
+        console.log('Desde getById budgetRouter')
+    }
+
+    static updateById = async (req: Request, res: Response) => {
+        console.log('Desde updateById budgetRouter')
+    }
+
+    static deleteById = async (req: Request, res: Response) => {
+        console.log('Desde deleteById budgetRouter')
+    }
+}
--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -1,9 +1,26 @@
-import { Router} from 'express'
+import { Router } from 'express'
+import { body } from 'express-validator'
+import { BudgetController } from '../controllers/BudgetController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    console.log('Desde budgetRouter')
-})
+router.get('/', BudgetController.getAll)
+
+router.post('/',
+    body('name')
+        .notEmpty().withMessage('El nombre del presupuesto no puede ir vacio '),
+    body('amount')
+        .notEmpty().withMessage('La cantidad del presupuesto no puede ir vacia ')
+        .isNumeric().withMessage('Cantidad no valida')
+        .custom(value => value>0).withMessage('Presupuesto debe ser mayor a 0'),
+    handleInputErrors,
+    BudgetController.create)
+
+router.get('/:id', BudgetController.getById)
+router.put('/:id', BudgetController.updateById)
+router.delete('/:id', BudgetController.deleteById)
+
+
 
 export default router
