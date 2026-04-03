9e9e7c819052b9043cd0e05dcfa953f2540fde43
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-04-03 17:22:40 -0600

    Creación de testing de middleware

---
+++ backend/src/test/unit/middleware/budget.test.ts
@@ -0,0 +1,93 @@
+import { createRequest, createResponse } from 'node-mocks-http'
+import { hasAccess, validateBudgetExists } from '../../../middleware/budget'
+import Budget from '../../../models/Budget'
+import { budgets } from '../../mocks/budget'
+
+jest.mock('../../../models/Budget', () => ({
+    __esModule: true,
+    default: {
+        findByPk: jest.fn()
+    }
+}))
+
+describe('Budget Middleware - validateBudgetExists', () => {
+    it('should handle non-existent budget', async () => {
+        (Budget.findByPk as jest.Mock).mockResolvedValue(null)
+        const req = createRequest({
+            params: {
+                budgetId: 1
+            }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+
+        await validateBudgetExists(req, res, next)
+        const data = res._getJSONData()
+
+        expect(res.statusCode).toBe(404)
+        expect(data).toEqual({ error: 'Presupuesto No Encontrado' })
+        expect(next).not.toHaveBeenCalled()
+    })
+
+    it('should proceed to next middleware if budget exists', async () => {
+        (Budget.findByPk as jest.Mock).mockResolvedValue(budgets[0])
+        const req = createRequest({
+            params: {
+                budgetId: 1
+            }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+
+        await validateBudgetExists(req, res, next)
+        expect(next).toHaveBeenCalled()
+        expect(req.budget).toEqual(budgets[0])
+    })
+
+    it('should handle erros on budgets', async () => {
+        (Budget.findByPk as jest.Mock).mockRejectedValue(new Error)
+        const req = createRequest({
+            params: {
+                budgetId: 1
+            }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+
+        await validateBudgetExists(req, res, next)
+        const data = res._getJSONData()
+
+        expect(res.statusCode).toBe(500)
+        expect(data).toEqual({ error: 'Hubo un error' })
+        expect(next).not.toHaveBeenCalled()
+    })
+})
+
+describe('Budget Middleware - hasAccess', () => {
+    it('Should call next() if user has access to budget', () => {
+        const req = createRequest({
+            budget: budgets[0],
+            user: { id: 1 }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+
+        hasAccess(req, res, next)
+        expect(next).toHaveBeenCalled()
+        expect(next).toHaveBeenCalledTimes(1)
+    })
+
+    it('Should return 401 error if userId does not have access to budget', () => {
+        const req = createRequest({
+            budget: budgets[0],
+            user: { id: 2 }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+
+        hasAccess(req, res, next)
+        expect(next).not.toHaveBeenCalled()
+        expect(res.statusCode).toBe(401)
+        expect(res._getJSONData()).toEqual({error: 'Acción no válida'})
+    })
+})
