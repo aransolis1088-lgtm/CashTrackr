2b9ab86b017384235fc09b5e3a8b25a26ef63ef2
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-04-13 21:01:20 -0600

    Finalización pruebas integración creación usuarios

--- backend/src/test/integration/app.test.ts
+++ backend/src/test/integration/app.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import server, { connectDB } from '../../server'
+import { AuthController } from '../../controllers/AuthController'
 
 describe('Authentication - Create Account', () => {
     it('Should display validation errors when form is empty', async () => {
@@ -7,9 +8,92 @@ describe('Authentication - Create Account', () => {
             .post('/api/auth/create-account')
             .send({})
 
+        const createAccountMock = jest.spyOn(AuthController, 'createAccount')
+
+
         expect(response.status).toBe(400)
         expect(response.body).toHaveProperty('errors')
         expect(response.body.errors).toHaveLength(3)
         expect(response.status).not.toBe(201)
+        expect(createAccountMock).not.toHaveBeenCalled()
+    })
+
+    it('Should return 400 status code when the email is invalid', async () => {
+        const response = await request(server)
+            .post('/api/auth/create-account')
+            .send({
+                "name": "Juan",
+                "password": "12345678",
+                "email": "not_valid_email"
+            })
+
+        const createAccountMock = jest.spyOn(AuthController, 'createAccount')
+
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+        expect(response.body.errors[0].msg).toBe('E-mail no válido')
+
+        expect(response.status).not.toBe(201)
+        expect(createAccountMock).not.toHaveBeenCalled()
+    })
+
+    it('Should return 400 status code when the password is less than 8 characters', async () => {
+        const response = await request(server)
+            .post('/api/auth/create-account')
+            .send({
+                "name": "Juan",
+                "password": "short",
+                "email": "test@test.com"
+            })
+
+        const createAccountMock = jest.spyOn(AuthController, 'createAccount')
+
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.body.errors[0].msg).toBe('El password es muy corto, mínimo 8 caracteres')
+        expect(response.status).not.toBe(201)
+        expect(createAccountMock).not.toHaveBeenCalled()
+    })
+
+    it('Should register a new user successfully', async () => {
+
+        const userData = {
+            "name": "Juan",
+            "password": "password",
+            "email": "test@test.com"
+        }
+
+        const response = await request(server)
+            .post('/api/auth/create-account')
+            .send(userData)
+
+        expect(response.status).toBe(201)
+        expect(response.body).not.toHaveProperty('errors')
+        expect(response.status).not.toBe(400)
+    })
+
+    it('Should returned 409 status code when a user is already registered', async () => {
+
+        const userData = {
+            "name": "Juan",
+            "password": "password",
+            "email": "test@test.com"
+        }
+
+        const response = await request(server)
+            .post('/api/auth/create-account')
+            .send(userData)
+
+        expect(response.status).toBe(409)
+        expect(response.status).not.toBe(400)
+        expect(response.status).not.toBe(201)
+        expect(response.body).not.toHaveProperty('errors')
+        expect(response.body).toHaveProperty('error')
+        expect(response.body.error).toBe('Un usuario con ese email ya está registrado.')
     })
 })
