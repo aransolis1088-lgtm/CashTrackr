288ea5e8dc884cf7c04368c58dcda86b23b671ee
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-04-19 19:43:28 -0600

    Creando pagina login y  registro

---
+++ frontend/app/auth/login/page.tsx
@@ -0,0 +1,19 @@
+import LoginForm from '@/components/auth/LoginForm'
+import type {Metadata} from 'next'
+
+export const metadata: Metadata = {
+    title: "CashTrackr - Iniciar Sesión",
+    description: "CashTrackr - Iniciar Sesión",
+}
+
+export default function LoginPage() {
+
+    return (
+        <>
+            <h1 className="font-black text-6xl text-purple-950">Iniciar Sesión</h1>
+            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
+
+            <LoginForm />
+        </>
+    )
+}
