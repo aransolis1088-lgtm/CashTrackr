a2094ea56231396cffcaf5f7d1bed05a916515cd
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-04-20 21:04:54 -0600

    Creando forgot password

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -10,8 +10,8 @@ export default function RegisterPage() {
 
     return (
         <>
-            <h1 className="font-black text-6xl text-purple-950">Crear una cuenta</h1>
-            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
+            <h1 className="font-black text-6xl text-purple-950">Crea una Cuenta</h1>
+            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p> 
 
             <RegisterForm />
         </>
