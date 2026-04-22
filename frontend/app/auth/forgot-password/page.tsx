f55fe07411210835e5e052c4b01e6808b887a6f4
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-04-21 20:31:52 -0600

    Implementación de routing en next.js

--- frontend/app/auth/forgot-password/page.tsx
+++ frontend/app/auth/forgot-password/page.tsx
@@ -1,5 +1,6 @@
 import ForgotPasswordForm from '@/components/auth/forgotPasswordForm'
 import type { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
     title: "CashTrackr - Olvidé mi Password",
@@ -14,6 +15,22 @@ export default function ForgotPasswordPage() {
             <p className="text-3xl font-bold">aquí puedes <span className="text-amber-500">reestablecerla</span></p>
 
             <ForgotPasswordForm />
+
+            <nav className='mt-10 flex flex-col space-y-4'>
+                <Link
+                    href='/auth/login'
+                    className='text-center text-gray-500'
+                >
+                    ¿Ya tienes cuenta? Iniciar Sesión
+                </Link>
+
+                <Link
+                    href='/auth/register'
+                    className='text-center text-gray-500'
+                >
+                    ¿No tienes cuenta? Crea una
+                </Link>
+            </nav>
 
         </>
     )
