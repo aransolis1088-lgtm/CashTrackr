45157498b60c0d53535dbe9de6bb94e03bcd1c5c
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-04-24 22:23:11 -0600

    Avance confirmación cuenta token

---
+++ frontend/components/auth/ConfirmAccountForm.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { PinInput, PinInputField } from '@chakra-ui/pin-input'
+import { useState } from 'react'
+
+export default function ConfirmAccountForm() {
+
+    const [token, setToken] = useState("")
+
+    const handleChange = (token: string) => {
+        setToken(token)
+    }
+
+    const handleComplete = () => {
+        
+    }
+
+    return (
+        <div className='flex justify-center gap-5 my-10'>
+            <PinInput
+                value={token}
+                onChange={handleChange}
+                onComplete={handleComplete}
+            >
+                <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg placeholder-white text-center' />
+
+                <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg placeholder-white text-center' />
+
+                <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg placeholder-white text-center' />
+
+                <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg placeholder-white text-center' />
+
+                <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg placeholder-white text-center' />
+
+                <PinInputField className='h-10 w-10 border border-gray-300 shadow rounded-lg placeholder-white text-center' />
+            </PinInput>
+            
+
+        </div>
+    )
+}
