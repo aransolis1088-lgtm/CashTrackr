faba4effceebf0806c35003e5a2c1d2cdd4fd2f8
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-05-15 18:28:43 -0600

    Mostrar modal para eliminar budget

--- frontend/components/budgets/BudgetMenu.tsx
+++ frontend/components/budgets/BudgetMenu.tsx
@@ -1,13 +1,16 @@
 "use client"
-import { Fragment } from "react"
+import { Fragment, use } from "react"
 import Link from "next/link"
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react"
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
 import { Budget } from "@/src/schemas"
+import { useRouter } from "next/navigation"
 
 
 
 export default function BudgetMenu({ budgetId }: { budgetId: Budget['id'] }) {
+
+    const router = useRouter()
     return (
         <>
             <Menu as="div" className="relative flex-none">
@@ -46,7 +49,7 @@ export default function BudgetMenu({ budgetId }: { budgetId: Budget['id'] }) {
                             <button
                                 type='button'
                                 className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                onClick={() => { }}
+                                onClick={() => router.push(`?deleteBudgetId=${budgetId}`)}
                             >
                                 Eliminar Presupuesto
                             </button>
