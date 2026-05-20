331cadc8faaf6f2038702a3ac6a0c6e8d8de34e7
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-05-19 18:21:45 -0600

    Implementación de Route Handler

---
+++ frontend/app/admin/api/budgets/[budgetId]/expenses/[expenseId]/route.ts
@@ -0,0 +1,21 @@
+import { verifySession } from "@/src/auth/dal"
+import getToken from "@/src/auth/token";
+
+export async function GET(request: Request, { params }: { params: { budgetId: string, expenseId: string } }) {
+    await verifySession()
+
+    const token = getToken();
+    const url = `${process.env.API_URL}/budgets/${params.budgetId}/expenses/${params.expenseId}`
+    const req = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const json = await req.json()
+
+    if (!req.ok) {
+        return Response.json(json.error)
+    }
+    return Response.json(json)
+}
