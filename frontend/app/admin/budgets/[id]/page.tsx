e261c5e76ba3ade74c15add749f89c41273d4aee
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-05-21 21:22:19 -0600

    feat - Mostrando total gastado

--- frontend/app/admin/budgets/[id]/page.tsx
+++ frontend/app/admin/budgets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import AddExpenseButton from "@/components/expenses/AddExpenseButton";
 import ExpenseMenu from "@/components/expenses/ExpenseMenu";
+import Amount from "@/components/ui/Amount";
 import ModalContainer from "@/components/ui/ModalContainer";
 import { getBudget } from "@/src/services/budgets"
 import { formatCurrency, formatDate } from "@/src/utils";
@@ -18,6 +19,9 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
 export default async function BudgetDetailsPage({ params }: { params: { id: string } }) {
     const budget = await getBudget(params.id);
 
+    const totalSpent = budget.expenses.reduce((total, expense) => +expense.amount + total, 0);
+    const totalAvailable = +budget.amount - totalSpent;
+
     return (
         <>
             <div className='flex justify-between items-center'>
@@ -30,6 +34,24 @@ export default async function BudgetDetailsPage({ params }: { params: { id: stri
 
             {budget.expenses.length ? (
                 <>
+                    <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
+                        <div></div>
+                        <div className="flex flex-col justify-center items-center md:items-start gap-5">
+                            <Amount
+                                label="Presupuesto"
+                                amount={+budget.amount}
+                            />
+                            <Amount
+                                label="Disponible"
+                                amount={totalAvailable}
+                            />
+                            <Amount
+                                label="Gastado"
+                                amount={totalSpent}
+                            />
+
+                        </div>
+                    </div>
                     <h1 className="font-black text-4xl text-purple-950 mt-10">
                         Gastos en este presupuesto
                     </h1>
