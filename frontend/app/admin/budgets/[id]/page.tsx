cbfb95a096a5f6f67313a3a46a9af1d5bc9678c9
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-05-22 21:18:06 -0600

    feat - Agregando grafica de gastos

--- frontend/app/admin/budgets/[id]/page.tsx
+++ frontend/app/admin/budgets/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ProgressBar from "@/components/budgets/ProgressBar";
 import AddExpenseButton from "@/components/expenses/AddExpenseButton";
 import ExpenseMenu from "@/components/expenses/ExpenseMenu";
 import Amount from "@/components/ui/Amount";
@@ -22,6 +23,8 @@ export default async function BudgetDetailsPage({ params }: { params: { id: stri
     const totalSpent = budget.expenses.reduce((total, expense) => +expense.amount + total, 0);
     const totalAvailable = +budget.amount - totalSpent;
 
+    const percentage = +((totalSpent / +budget.amount) * 100).toFixed(2)
+
     return (
         <>
             <div className='flex justify-between items-center'>
@@ -35,7 +38,9 @@ export default async function BudgetDetailsPage({ params }: { params: { id: stri
             {budget.expenses.length ? (
                 <>
                     <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
-                        <div></div>
+                        <ProgressBar
+                            percentage={percentage}
+                        />
                         <div className="flex flex-col justify-center items-center md:items-start gap-5">
                             <Amount
                                 label="Presupuesto"
